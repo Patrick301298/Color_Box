59e890b7c534f9be56aefd139e8e39e257ec9927
Le Xuan Truong <lxtgl1998@gmail.com>
2021-06-08 11:37:22 +0700

    new TodoForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.scss";
+import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
@@ -18,9 +19,20 @@ function App() {
     newTodoList.splice(index, 1);
     setTodoList(newTodoList);
   }
+  function handleTodoFormSubmit(formValues) {
+    console.log("Form Submit ", formValues);
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="app">
       <h1> Welcome -TodoList</h1>
+      <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
